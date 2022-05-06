d1aedac7d9676b5a1ff5a9fe3ec9ef85fbe1e8d0
khadiza21 <bibikhadiza474@gmail.com>
2022-05-06 19:24:36 +0600

    delete function done

--- src/components/Inventories/Inventories.js
+++ src/components/Inventories/Inventories.js
@@ -1,23 +1,52 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Inventory from "../Inventory/Inventory";
+
 import useInventory from "../Shared/hooks/useInventory";
 
 const Inventories = () => {
-  const [inventories] = useInventory();
+  const [inventories,setInventories] = useInventory();
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are You Sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/inventory/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((deleteItem) =>{ 
+          console.log(deleteItem);
+          const remaining = inventories.filter(inventory => inventory._id !== id);
+          setInventories(remaining);
+        });
+    }
+  };
   return (
     <div>
-      <div className=" container inventories-container my-5 pb-5">
+      <div className=" container  my-5 pb-5">
+        <div className="fw-bold fs-2 my-5 text-center">Manage All Inventories</div>
+        <div className="inventories-container">
         {inventories.map((inventory) => (
-          <Inventory key={inventory._id} inventory={inventory}></Inventory>
-        ))}
+          <div key={inventory._id} className="border rounded">
+            <ul>
+              <li>{inventory.name}</li>
+              <li>{inventory.supplierName}</li>
+              <li>{inventory.quantity}</li>
+            </ul>
+            <button className="btn btn-info m-3" onClick={() => handleDelete(inventory._id)}>
+              Delete Items
+            </button>
+          </div>
+        ))}</div>
       </div>
 
       <div>
-      <Link className="d-flex justify-content-center my-5 py-5" to="/addInventory">
-        <Button>Add New Inventory</Button>
-      </Link>
+        <Link
+          className="d-flex justify-content-center my-5 py-5"
+          to="/addInventory"
+        >
+          <Button>Add New Inventory</Button>
+        </Link>
       </div>
     </div>
   );
